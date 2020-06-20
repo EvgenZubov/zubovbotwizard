d940e9ca7f8d248db817cb9dda27d132084ba0fd
nhrin <grinkonu@gmail.com>
2020-06-20 11:57:34 +0300

    init

---
+++ src/main/java/mytestwizardbot/zubovbotwizard/ZubovbotwizardApplication.java
@@ -0,0 +1,13 @@
+package mytestwizardbot.zubovbotwizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZubovbotwizardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZubovbotwizardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mytestwizardbot/zubovbotwizard/ZubovbotwizardApplicationTests.java
@@ -0,0 +1,13 @@
+package mytestwizardbot.zubovbotwizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZubovbotwizardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
